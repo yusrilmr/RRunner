3ef262f4a3694adc37d2f838eabaf749614be8a1
yusrilmr <yusrilmaulidanraji@gmail.com>
2015-04-01 23:03:24 +0200

    Lobby

--- src/client/Lobby.java
+++ src/client/Lobby.java
@@ -1,5 +1,30 @@
 package client;
 
-public class Lobby {
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class Lobby extends JFrame {
+	DefaultTableModel model = new DefaultTableModel(new Object[][] {
+			{ "some", "text" }, { "any", "text" }, { "even", "more" },
+		      { "text", "strings" }, { "and", "other" }, { "text", "values" } },
+	new Object[]{"Column 1", "Column 2"});
+	
+	public Lobby(){
+		setSize(500, 500);
+		JTable table = new JTable(model);
+		getContentPane().setLayout(null);
+		
+		table.setBounds(120, 23, 345, 50);
+		getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		Lobby lobby = new Lobby();
+	}
 }
