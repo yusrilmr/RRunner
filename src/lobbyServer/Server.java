8d0d0b51cb45588c9cb28bd549232d0ba61df580
yusrilmr <yusrilmaulidanraji@gmail.com>
2015-04-02 12:25:33 +0200

    Model User

--- src/lobbyServer/Server.java
+++ src/lobbyServer/Server.java
@@ -1,4 +1,6 @@
 package lobbyServer;
+import java.util.ArrayList;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -12,29 +14,33 @@ public class Server {
     public static void main(String[] args) throws InterruptedException {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup(10);
+        
+        //String coba = "";
 
         try {
-        	System.out.println("1");
             ServerBootstrap b = new ServerBootstrap();
-            System.out.println("2");
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .option(ChannelOption.SO_BACKLOG, 100)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
-                        	System.out.println("3");
+                        	//ArrayList<String> coba = new ArrayList<String>();
+                        	String Var = "";
                             ChannelPipeline p = ch.pipeline();
-                            System.out.println("4");
+                            
+                            
+                            //Var = p.addLast(new ServerHandler()).toString();
                             p.addLast(new ServerHandler());
-                            System.out.println("5");
+                            
+                            //System.out.println("Try Variable: "+Var);
+                           // System.out.println("nyoba "+coba.get(0));
+                        //    Var = p.addLast(new ServerHandler());
+                           
                         }
                     });
-            System.out.println("6");
             ChannelFuture f = b.bind(11111).sync();
-            System.out.println("7");
             f.channel().closeFuture().sync();
-            System.out.println("8");
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
--- src/lobbyServer/ServerHandler.java
+++ src/lobbyServer/ServerHandler.java
@@ -1,18 +1,29 @@
 package lobbyServer;
+import java.util.ArrayList;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class ServerHandler extends ChannelInboundHandlerAdapter {
-
+ArrayList<String> ArrayListUsername = new ArrayList<String>();
+	
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf message = (ByteBuf) msg;
 
         byte[] bytes = new byte[message.readableBytes()];
         message.readBytes(bytes);
-
-        System.out.println(String.format("%s send %s.", ctx.channel().remoteAddress(), new String(bytes)));
+     
+        ArrayListUsername.add(new String(bytes));
+        
+        for(int i=0;i<ArrayListUsername.size();i++){
+        	System.out.println("array ke "+i+" "+ArrayListUsername.get(i));
+        }
+        
+        //System.out.println(new String(bytes));
+       // System.out.println(String.format("%s send %s.", ctx.channel().remoteAddress(), new String(bytes)));
+ 
     }
 
     @Override
