d9b2793abd8f289c501e84bfc43715d1ad780496
yusrilmr <yusrilmaulidanraji@gmail.com>
2015-04-02 11:08:29 +0200

    t

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,49 @@
 package client;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Scanner;
 
 public class Client {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            final ClientHandler handler = new ClientHandler();
+
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(handler);
+                        }
+                    });
+            //use ip local (for now)
+            ChannelFuture f = b.connect("145.93.49.62", 11111).sync();
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (true) {
+                String message = scanner.nextLine();
+
+                if (message.equals("Exit")) {
+                    break;
+                } else {
+                    handler.send(message);
+                }
+            }
+
+            f.channel().close();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
 
 }
